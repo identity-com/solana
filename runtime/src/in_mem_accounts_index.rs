0de16c3b9aeb1d2f07f5dfa51e1ef47ffb0f0b6e
frits-double-eye <31902851+frits-double-eye@users.noreply.github.com>
2021-11-24 14:39:20 +0200

    Revert "InMemAccountsIndex iter returns vec<owned> (#19778)"
    This reverts commit e9cca0e98f0838b2183ae9da76ec688e3b79c1ca.

--- runtime/src/in_mem_accounts_index.rs
+++ runtime/src/in_mem_accounts_index.rs
@@ -1,7 +1,7 @@
 use crate::accounts_index::{AccountMapEntry, IsCached};
 use solana_sdk::pubkey::Pubkey;
 use std::collections::{
-    hash_map::{Entry, Keys},
+    hash_map::{Entry, Iter, Keys},
     HashMap,
 };
 use std::fmt::Debug;
@@ -26,8 +26,8 @@ impl<V: IsCached> InMemAccountsIndex<V> {
         self.map.entry(pubkey)
     }
 
-    pub fn items(&self) -> Vec<(K, AccountMapEntry<V>)> {
-        self.map.iter().map(|(k, v)| (*k, v.clone())).collect()
+    pub fn iter(&self) -> Iter<K, AccountMapEntry<V>> {
+        self.map.iter()
     }
 
     pub fn keys(&self) -> Keys<K, AccountMapEntry<V>> {
