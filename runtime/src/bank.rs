0d2a8ce8ac16f0e51e141b816f13db2f82f6fe3c
frits-double-eye <31902851+frits-double-eye@users.noreply.github.com>
2021-11-24 14:39:20 +0200

    Revert "filler accts: only add filler accts to slots in the current epoch (#21024)"
    This reverts commit 8fa56e6efb0311f32474b56ae236988c1ecf127b.

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -4546,8 +4546,6 @@ impl Bank {
         Self::get_partitions(self.slot(), self.parent_slot(), slot_count_in_two_day)
     }
 
-    /// used only by filler accounts in debug path
-    /// previous means slot - 1, not parent
     pub fn variable_cycle_partition_from_previous_slot(
         epoch_schedule: &EpochSchedule,
         slot: Slot,
@@ -4632,7 +4630,7 @@ impl Bank {
         )
     }
 
-    fn get_partition_from_slot_indexes(
+    pub fn get_partition_from_slot_indexes(
         cycle_params: RentCollectionCycleParams,
         start_slot_index: SlotIndex,
         end_slot_index: SlotIndex,
@@ -4707,7 +4705,7 @@ impl Bank {
         self.do_partition_from_slot_indexes(start_slot_index, end_slot_index, epoch, true)
     }
 
-    fn rent_single_epoch_collection_cycle_params(
+    pub fn rent_single_epoch_collection_cycle_params(
         epoch: Epoch,
         slot_count_per_epoch: SlotCount,
     ) -> RentCollectionCycleParams {
