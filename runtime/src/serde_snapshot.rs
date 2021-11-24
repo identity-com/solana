a9487784154664a55e2f2db63d85d295b5dbaa7c
frits-double-eye <31902851+frits-double-eye@users.noreply.github.com>
2021-11-24 14:39:20 +0200

    Revert "Constrain memory ordering on AccountsDb::write_version (#21312)"
    This reverts commit 37185aceed2b5c2ef3e3744f1e0e5797fe74fc20.

--- runtime/src/serde_snapshot.rs
+++ runtime/src/serde_snapshot.rs
@@ -536,7 +536,7 @@ where
         .store(next_append_vec_id, Ordering::Relaxed);
     accounts_db
         .write_version
-        .fetch_add(snapshot_version, Ordering::Release);
+        .fetch_add(snapshot_version, Ordering::Relaxed);
 
     let mut measure_notify = Measure::start("accounts_notify");
 
