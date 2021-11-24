165e512dabc0a9783a9a281cfbb0796282b275da
frits-double-eye <31902851+frits-double-eye@users.noreply.github.com>
2021-11-24 14:39:20 +0200

    Revert "fixing print in shell script and updating string interpolation in python script"
    This reverts commit c2ce4c27338a0bb0bf90d338644d951c3df1daa0.

--- scripts/perf-plot.py
+++ scripts/perf-plot.py
@@ -11,7 +11,7 @@ stages_to_counters = {}
 stages_to_time = {}
 
 if len(sys.argv) != 2:
-    print(f"USAGE: {sys.argv[0]} <input file>")
+    print("USAGE: {} <input file>".format(sys.argv[0]))
     sys.exit(1)
 
 with open(sys.argv[1]) as fh:
