4a3dc56b56bff3cca4e8dec716b2b4d035dff766
frits-double-eye <31902851+frits-double-eye@users.noreply.github.com>
2021-11-24 14:39:20 +0200

    Revert "Added better layout suppport in MetaplexNFTHeader for mobile devices (#20705)"
    This reverts commit d140c7836ea970850f00134ffd3eade1e4dc80bb.

--- explorer/src/components/account/MetaplexNFTHeader.tsx
+++ explorer/src/components/account/MetaplexNFTHeader.tsx
@@ -16,21 +16,22 @@ export function NFTHeader({
 }) {
   const metadata = nftData.metadata;
   return (
-    <div className="row">
+    <div className="row align-items-begin">
       <div className="col-auto ml-2 d-flex align-items-center">
         <ArtContent metadata={metadata} pubkey={address} />
       </div>
-      <div className="col mb-3 ml-0.5 mt-3">
+
+      <div className="col mb-3 ml-n3 ml-md-n2 mt-3">
         {<h6 className="header-pretitle ml-1">Metaplex NFT</h6>}
         <div className="d-flex align-items-center">
-          <h2 className="header-title ml-1 align-items-center no-overflow-with-ellipsis">
+          <h2 className="header-title ml-1 align-items-center">
             {metadata.data.name !== ""
               ? metadata.data.name
               : "No NFT name was found"}
           </h2>
           {getEditionPill(nftData.editionInfo)}
         </div>
-        <h4 className="header-pretitle ml-1 mt-1 no-overflow-with-ellipsis">
+        <h4 className="header-pretitle ml-1 mt-1">
           {metadata.data.symbol !== ""
             ? metadata.data.symbol
             : "No Symbol was found"}
