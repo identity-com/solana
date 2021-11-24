9bbd5092442922b3dd522cca83dcceb8c7b15b8e
frits-double-eye <31902851+frits-double-eye@users.noreply.github.com>
2021-11-24 14:39:20 +0200

    Revert "Added Prettier fixes for NFTArt (#20752)"
    This reverts commit 132a2eddb411e931c8493208eba2272b58c09002.

--- explorer/src/components/common/NFTArt.tsx
+++ explorer/src/components/common/NFTArt.tsx
@@ -40,7 +40,7 @@ const ViewOriginalArtContentLink = ({ src }: { src: string }) => {
       <a href={src}>VIEW ORIGINAL</a>
     </h6>
   );
-};
+}
 
 const CachedImageContent = ({ uri }: { uri?: string }) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -93,7 +93,9 @@ const CachedImageContent = ({ uri }: { uri?: string }) => {
                 setShowError(true);
               }}
             />
-            {uri && <ViewOriginalArtContentLink src={uri} />}
+            {uri && (
+              <ViewOriginalArtContentLink src={uri} />
+            )}
           </div>
         </>
       )}
@@ -137,7 +139,7 @@ const VideoArtContent = ({
 
   const content =
     likelyVideo &&
-    likelyVideo.startsWith("https://watch.videodelivery.net/") ? (
+      likelyVideo.startsWith("https://watch.videodelivery.net/") ? (
       <div className={"d-block"}>
         <Stream
           streamRef={(e: any) => playerRef(e)}
@@ -154,9 +156,7 @@ const VideoArtContent = ({
           autoplay={true}
           muted={true}
         />
-        <ViewOriginalArtContentLink
-          src={likelyVideo.replace("https://watch.videodelivery.net/", "")}
-        />
+        <ViewOriginalArtContentLink src={likelyVideo.replace("https://watch.videodelivery.net/", "")} />
       </div>
     ) : (
       <div className={"d-block"}>
@@ -182,7 +182,7 @@ const VideoArtContent = ({
           <ViewOriginalArtContentLink src={(likelyVideo || animationURL)!} />
         )}
       </div>
-    );
+    )
 
   return content;
 };
@@ -226,7 +226,9 @@ const HTMLContent = ({
                 setShowError(true);
               }}
             ></iframe>
-            {htmlURL && <ViewOriginalArtContentLink src={htmlURL} />}
+            {htmlURL && (
+              <ViewOriginalArtContentLink src={htmlURL} />
+            )}
           </div>
         </>
       )}
