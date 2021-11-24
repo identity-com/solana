d17d53689dcb5ade26aa88ef97f9d95b54a5a1c4
frits-double-eye <31902851+frits-double-eye@users.noreply.github.com>
2021-11-24 14:39:20 +0200

    Revert "Add transaction mode to dos (#20191)"
    This reverts commit c4140514631c1ab85971f17cc7436b1886b3d917.

--- dos/src/main.rs
+++ dos/src/main.rs
@@ -79,10 +79,6 @@ fn run_dos(
             "random" => {
                 data.resize(data_size, 0);
             }
-            "transaction" => {
-                let tx = solana_perf::test_tx::test_tx();
-                data = bincode::serialize(&tx).unwrap();
-            }
             "get_account_info" => {}
             "get_program_accounts" => {}
             &_ => {
@@ -187,7 +183,6 @@ fn main() {
                     "random",
                     "get_account_info",
                     "get_program_accounts",
-                    "transaction",
                 ])
                 .help("Type of data to send"),
         )
