58729a9de3e3d1b896e3c2e6f85704bf8c57908d
frits-double-eye <31902851+frits-double-eye@users.noreply.github.com>
2021-11-24 14:39:20 +0200

    Revert "AcctIdx: set age of cache accesses to future (#20125)"
    This reverts commit 51942a09c71cf0d9eef09a77a9f609c56f7ece3a.

--- runtime/src/in_mem_accounts_index.rs
+++ runtime/src/in_mem_accounts_index.rs
@@ -142,8 +142,7 @@ impl<T: IndexValue> InMemAccountsIndex<T> {
         Self::update_time_stat(time, m);
         Self::update_stat(count, 1);
 
-        if let Some(entry) = result.as_ref() {
-            entry.set_age(self.storage.future_age_to_flush());
+        if result.is_some() {
             return result;
         }
 
@@ -259,7 +258,6 @@ impl<T: IndexValue> InMemAccountsIndex<T> {
                     reclaims,
                     previous_slot_entry_was_cached,
                 );
-                current.set_age(self.storage.future_age_to_flush());
                 Self::update_stat(&self.stats().updates_in_mem, 1);
             }
             Entry::Vacant(vacant) => {
@@ -538,13 +536,11 @@ impl<T: IndexValue> InMemAccountsIndex<T> {
         if let Some(disk) = self.storage.disk.as_ref() {
             let items = disk.items_in_range(self.bin, range);
             let mut map = self.map().write().unwrap();
-            let future_age = self.storage.future_age_to_flush();
             for item in items {
                 let entry = map.entry(item.pubkey);
                 match entry {
-                    Entry::Occupied(occupied) => {
-                        // item already in cache, bump age to future. This helps the current age flush to succeed.
-                        occupied.get().set_age(future_age);
+                    Entry::Occupied(_occupied) => {
+                        // do nothing - item already in cache
                     }
                     Entry::Vacant(vacant) => {
                         vacant.insert(self.disk_to_cache_entry(item.slot_list, item.ref_count));
