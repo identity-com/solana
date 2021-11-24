6d8ea8197ee94e72d7be626cba10aba6181c3152
frits-double-eye <31902851+frits-double-eye@users.noreply.github.com>
2021-11-24 14:39:20 +0200

    Revert "Only gossip snapshot hashes for full snapshots (#20271)"
    This reverts commit 1c2a503f895f97ae89596b338f070b4f119e98b1.

--- core/src/snapshot_packager_service.rs
+++ core/src/snapshot_packager_service.rs
@@ -1,9 +1,7 @@
 use solana_gossip::cluster_info::{ClusterInfo, MAX_SNAPSHOT_HASHES};
 use solana_runtime::{
-    snapshot_archive_info::SnapshotArchiveInfoGetter,
-    snapshot_config::SnapshotConfig,
-    snapshot_package::{PendingSnapshotPackage, SnapshotType},
-    snapshot_utils,
+    snapshot_archive_info::SnapshotArchiveInfoGetter, snapshot_config::SnapshotConfig,
+    snapshot_package::PendingSnapshotPackage, snapshot_utils,
 };
 use solana_sdk::{clock::Slot, hash::Hash};
 use std::{
@@ -60,15 +58,11 @@ impl SnapshotPackagerService {
                     )
                     .expect("failed to archive snapshot package");
 
-                    // NOTE: For backwards compatibility with version <=1.7, only _full_ snapshots
-                    // can have their hashes pushed out to the cluster.
-                    if snapshot_package.snapshot_type == SnapshotType::FullSnapshot {
-                        hashes.push((snapshot_package.slot(), *snapshot_package.hash()));
-                        while hashes.len() > MAX_SNAPSHOT_HASHES {
-                            hashes.remove(0);
-                        }
-                        cluster_info.push_snapshot_hashes(hashes.clone());
+                    hashes.push((snapshot_package.slot(), *snapshot_package.hash()));
+                    while hashes.len() > MAX_SNAPSHOT_HASHES {
+                        hashes.remove(0);
                     }
+                    cluster_info.push_snapshot_hashes(hashes.clone());
                 }
             })
             .unwrap();
