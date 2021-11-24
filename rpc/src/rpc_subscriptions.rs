857b8c853f24fed7ed889336c13683ec1b173504
frits-double-eye <31902851+frits-double-eye@users.noreply.github.com>
2021-11-24 14:39:20 +0200

    Revert "Use config limit instead of default (#20900)"
    This reverts commit 3a02a83c771abe5e5d05a922f715afe3690c283a.

--- rpc/src/rpc_subscriptions.rs
+++ rpc/src/rpc_subscriptions.rs
@@ -499,7 +499,7 @@ impl RpcSubscriptions {
             .unwrap();
 
         let control = SubscriptionControl::new(
-            config.max_active_subscriptions,
+            PubSubConfig::default().max_active_subscriptions,
             notification_sender.clone(),
             broadcast_sender,
         );
